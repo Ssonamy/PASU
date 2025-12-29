49c89882b4488ad2d6e12d367f7b112ced765477
Ssonamy <lev.salnikovf@gmail.com>
2025-12-29 14:46:03 +0300

    Вроде бы готово к сдаче

---
+++ Lab_6.1/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Recieving_data
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lst_data = new System.Windows.Forms.ListBox();
+            this.start_btn = new System.Windows.Forms.Button();
+            this.stop_btn = new System.Windows.Forms.Button();
+            this.pause_btn = new System.Windows.Forms.Button();
+            this.cbx_choose_mode = new System.Windows.Forms.ComboBox();
+            this.txt_freq = new System.Windows.Forms.TextBox();
+            this.txt_port = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox_show = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_selectFile = new System.Windows.Forms.Button();
+            this.txt_name_file = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.fileTimer = new System.Windows.Forms.Timer(this.components);
+            this.scrollBarHistory = new System.Windows.Forms.HScrollBar();
+            this.labelHistory = new System.Windows.Forms.Label();
+
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+
+            // pictureBox1
+            this.pictureBox1.Location = new System.Drawing.Point(560, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(700, 520);
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+
+            // lst_data
+            this.lst_data.FormattingEnabled = true;
+            this.lst_data.ItemHeight = 15;
+            this.lst_data.Location = new System.Drawing.Point(10, 20);
+            this.lst_data.Name = "lst_data";
+            this.lst_data.Size = new System.Drawing.Size(540, 319);
+
+            // start_btn
+            this.start_btn.Location = new System.Drawing.Point(445, 460);
+            this.start_btn.Name = "start_btn";
+            this.start_btn.Size = new System.Drawing.Size(95, 40);
+            this.start_btn.Text = "Start";
+            this.start_btn.UseVisualStyleBackColor = true;
+            this.start_btn.Click += new System.EventHandler(this.start_btn_Click);
+
+            // stop_btn
+            this.stop_btn.Location = new System.Drawing.Point(445, 505);
+            this.stop_btn.Name = "stop_btn";
+            this.stop_btn.Size = new System.Drawing.Size(95, 40);
+            this.stop_btn.Text = "Stop";
+            this.stop_btn.UseVisualStyleBackColor = true;
+            this.stop_btn.Click += new System.EventHandler(this.stop_btn_Click);
+
+            // pause_btn
+            this.pause_btn.Location = new System.Drawing.Point(335, 395);
+            this.pause_btn.Name = "pause_btn";
+            this.pause_btn.Size = new System.Drawing.Size(95, 40);
+            this.pause_btn.Text = "Pause";
+            this.pause_btn.UseVisualStyleBackColor = true;
+            this.pause_btn.Click += new System.EventHandler(this.pause_btn_Click);
+
+            // cbx_choose_mode
+            this.cbx_choose_mode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbx_choose_mode.FormattingEnabled = true;
+            this.cbx_choose_mode.Items.AddRange(new object[] {
+                "считывание из файла",
+                "считывание из сети по UDP"});
+            this.cbx_choose_mode.Location = new System.Drawing.Point(35, 387);
+            this.cbx_choose_mode.Name = "cbx_choose_mode";
+            this.cbx_choose_mode.Size = new System.Drawing.Size(245, 23);
+
+            // txt_freq
+            this.txt_freq.Location = new System.Drawing.Point(16, 90);
+            this.txt_freq.Name = "txt_freq";
+            this.txt_freq.Size = new System.Drawing.Size(130, 23);
+
+            // txt_port
+            this.txt_port.Location = new System.Drawing.Point(15, 45);
+            this.txt_port.Name = "txt_port";
+            this.txt_port.Size = new System.Drawing.Size(90, 23);
+
+            // label1
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Text = "Порт:";
+
+            // label2
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 65);
+            this.label2.Text = "Интервал чтения (мс):";
+
+            // label3
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 368);
+            this.label3.Text = "Режим чтения:";
+
+            // checkBox_show
+            this.checkBox_show.AutoSize = true;
+            this.checkBox_show.Location = new System.Drawing.Point(335, 360);
+            this.checkBox_show.Text = "Показывать данные таблицы";
+
+            // groupBox2
+            this.groupBox2.Controls.Add(this.btn_selectFile);
+            this.groupBox2.Controls.Add(this.txt_name_file);
+            this.groupBox2.Controls.Add(this.txt_freq);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(20, 415);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(285, 130);
+            this.groupBox2.Text = "Работа с файлом";
+
+            // btn_selectFile
+            this.btn_selectFile.Location = new System.Drawing.Point(170, 85);
+            this.btn_selectFile.Name = "btn_selectFile";
+            this.btn_selectFile.Size = new System.Drawing.Size(95, 35);
+            this.btn_selectFile.Text = "Select File";
+            this.btn_selectFile.UseVisualStyleBackColor = true;
+            this.btn_selectFile.Click += new System.EventHandler(this.btn_selectFile_Click);
+
+            // txt_name_file
+            this.txt_name_file.Location = new System.Drawing.Point(16, 30);
+            this.txt_name_file.Name = "txt_name_file";
+            this.txt_name_file.Size = new System.Drawing.Size(250, 23);
+
+            // groupBox3
+            this.groupBox3.Controls.Add(this.txt_port);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Location = new System.Drawing.Point(315, 455);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(120, 90);
+            this.groupBox3.Text = "Работа с UDP";
+
+            // scrollBarHistory
+            this.scrollBarHistory.Location = new System.Drawing.Point(560, 545);
+            this.scrollBarHistory.Name = "scrollBarHistory";
+            this.scrollBarHistory.Size = new System.Drawing.Size(700, 17);
+
+            // labelHistory
+            this.labelHistory.AutoSize = true;
+            this.labelHistory.Location = new System.Drawing.Point(560, 528);
+            this.labelHistory.Text = "История:";
+
+            // Form1
+            this.ClientSize = new System.Drawing.Size(1275, 600);
+            this.Controls.Add(this.labelHistory);
+            this.Controls.Add(this.scrollBarHistory);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.checkBox_show);
+            this.Controls.Add(this.cbx_choose_mode);
+            this.Controls.Add(this.pause_btn);
+            this.Controls.Add(this.stop_btn);
+            this.Controls.Add(this.start_btn);
+            this.Controls.Add(this.lst_data);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Lidar visualizer";
+
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox lst_data;
+        private System.Windows.Forms.Button start_btn;
+        private System.Windows.Forms.Button stop_btn;
+        private System.Windows.Forms.Button pause_btn;
+        private System.Windows.Forms.ComboBox cbx_choose_mode;
+        private System.Windows.Forms.TextBox txt_freq;
+        private System.Windows.Forms.TextBox txt_port;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox_show;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_selectFile;
+        private System.Windows.Forms.TextBox txt_name_file;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Timer fileTimer;
+        private System.Windows.Forms.HScrollBar scrollBarHistory;
+        private System.Windows.Forms.Label labelHistory;
+    }
+}
---
+++ Lab_6.1/Form1.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Recieving_data
+{
+    public partial class Form1 : Form
+    {
+        // ===== МОДЕЛИ =====
+        private class LidarFrame
+        {
+            public IReadOnlyList<int> Distances { get; }
+            public DateTime Timestamp { get; }
+            public double Azimuth { get; }
+
+            public LidarFrame(List<int> distances, DateTime time, double azimuth)
+            {
+                Distances = distances;
+                Timestamp = time;
+                Azimuth = azimuth;
+            }
+        }
+
+        private class LidarFrameWithPos : LidarFrame
+        {
+            public double X { get; }
+            public double Y { get; }
+
+            public LidarFrameWithPos(
+                List<int> distances,
+                DateTime time,
+                double x,
+                double y,
+                double azimuth)
+                : base(distances, time, azimuth)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+
+        // ===== ПОЛЯ =====
+        private readonly ConcurrentQueue<LidarFrame> frameQueue = new();
+        private readonly List<LidarFrame> historyFrames = new();
+        private int historyIndex = -1;
+
+        private CancellationTokenSource cts;
+        private UdpClient udpServer;
+        private StreamReader reader;
+
+        private bool isPaused;
+        private List<int> currentFrame = new();
+        private DateTime currentTimeOfFrame;
+
+        // ===== МАСШТАБЫ =====
+        private const double DIST_SCALE = 50;
+        private const double POS_SCALE = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            cbx_choose_mode.SelectedIndex = 0;
+            cbx_choose_mode.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            txt_port.Text = "8080";
+            txt_freq.Text = "200";
+
+            btn_selectFile.Click += btn_selectFile_Click;
+            scrollBarHistory.Scroll += scrollBarHistory_Scroll;
+        }
+
+        // ===== КНОПКИ =====
+        private void start_btn_Click(object sender, EventArgs e)
+        {
+            if (cbx_choose_mode.SelectedIndex == 0)
+                StartFileReading();
+            else
+                StartUdpReceiving();
+        }
+
+        private void stop_btn_Click(object sender, EventArgs e) => StopAll();
+
+        private void pause_btn_Click(object sender, EventArgs e)
+        {
+            isPaused = !isPaused;
+            pause_btn.Text = isPaused ? "Continue" : "Pause";
+        }
+
+        // ===== ФАЙЛ =====
+        private void btn_selectFile_Click(object sender, EventArgs e)
+        {
+            using OpenFileDialog ofd = new OpenFileDialog
+            {
+                Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"
+            };
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+                txt_name_file.Text = ofd.FileName;
+        }
+
+        private void StartFileReading()
+        {
+            if (!File.Exists(txt_name_file.Text))
+                return;
+
+            StopAll();
+
+            reader = new StreamReader(txt_name_file.Text);
+            fileTimer.Interval = int.TryParse(txt_freq.Text, out int f) ? f : 200;
+            fileTimer.Start();
+        }
+
+        private void fileTimer_Tick(object sender, EventArgs e)
+        {
+            if (isPaused || reader == null || reader.EndOfStream)
+                return;
+
+            string json = reader.ReadLine();
+            if (TryParseJsonFrame(json, out var frame))
+            {
+                frameQueue.Enqueue(frame);
+                UpdateFromQueue();
+            }
+        }
+
+        // ===== UDP =====
+        private void StartUdpReceiving()
+        {
+            if (!int.TryParse(txt_port.Text, out int port))
+                return;
+
+            StopAll();
+
+            cts = new CancellationTokenSource();
+            udpServer = new UdpClient(port);
+
+            Task.Run(() => ReceiveUdpAsync(cts.Token));
+        }
+
+        private async Task ReceiveUdpAsync(CancellationToken token)
+        {
+            try
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    var result = await udpServer.ReceiveAsync();
+                    string json = Encoding.UTF8.GetString(result.Buffer).Trim();
+
+                    if (TryParseJsonFrame(json, out var frame))
+                    {
+                        frameQueue.Enqueue(frame);
+                        if (!isPaused)
+                            BeginInvoke(new Action(UpdateFromQueue));
+                    }
+                }
+            }
+            catch { }
+        }
+
+        // ===== JSON =====
+        private bool TryParseJsonFrame(string json, out LidarFrame frame)
+        {
+            frame = null;
+
+            try
+            {
+                var dict = JsonSerializer.Deserialize<Dictionary<string, string>>(json);
+                if (dict == null) return false;
+
+                var distances = dict
+                    .Where(k => k.Key.StartsWith("d"))
+                    .OrderBy(k => int.Parse(k.Key[1..]))
+                    .Select(k => int.TryParse(k.Value, out int v) ? v : -1)
+                    .Where(v => v > 0)
+                    .ToList();
+
+                if (distances.Count < 2) return false;
+
+                double x = dict.TryGetValue("x", out var xs) && double.TryParse(xs, out var xv) ? xv : 0;
+                double y = dict.TryGetValue("y", out var ys) && double.TryParse(ys, out var yv) ? yv : 0;
+                double az = dict.TryGetValue("azimuth", out var azs) && double.TryParse(azs, out var azv) ? azv : 0;
+
+                frame = new LidarFrameWithPos(distances, DateTime.Now, x, y, az);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        // ===== ОБНОВЛЕНИЕ =====
+        private void UpdateFromQueue()
+        {
+            while (frameQueue.TryDequeue(out var frame))
+            {
+                currentFrame = frame.Distances.ToList();
+                currentTimeOfFrame = frame.Timestamp;
+
+                historyFrames.Add(frame);
+                historyIndex = historyFrames.Count - 1;
+            }
+
+            UpdateScrollbar();
+            UpdateDisplay();
+        }
+
+        private void UpdateScrollbar()
+        {
+            scrollBarHistory.Minimum = 0;
+            scrollBarHistory.Maximum = Math.Max(0, historyFrames.Count - 1);
+            scrollBarHistory.Value = historyIndex;
+        }
+
+        private void scrollBarHistory_Scroll(object sender, ScrollEventArgs e)
+        {
+            historyIndex = scrollBarHistory.Value;
+            DrawLidar();
+        }
+
+        private void UpdateDisplay()
+        {
+            if (checkBox_show.Checked)
+            {
+                lst_data.Items.Add(
+                    $"[{currentTimeOfFrame:HH:mm:ss.fff}] {currentFrame.Count} точек");
+                lst_data.SelectedIndex = lst_data.Items.Count - 1;
+            }
+
+            DrawLidar();
+        }
+
+        // ===== ОТРИСОВКА =====
+        private void DrawLidar()
+        {
+            if (historyFrames.Count == 0) return;
+
+            int w = pictureBox1.Width;
+            int h = pictureBox1.Height;
+
+            Bitmap bmp = new Bitmap(w, h);
+            using Graphics g = Graphics.FromImage(bmp);
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            g.Clear(Color.White);
+
+            int cx = w / 2;
+            int cy = h / 2;
+
+            using Pen dashedPen = new Pen(Color.LightGray, 1)
+            {
+                DashStyle = DashStyle.Dash
+            };
+
+            using SolidBrush pointBrush = new(Color.Red);
+
+            int startIndex = Math.Max(0, historyIndex - 20);
+
+            for (int f = startIndex; f <= historyIndex; f++)
+            {
+                if (historyFrames[f] is not LidarFrameWithPos frame)
+                    continue;
+
+                double step = 360.0 / frame.Distances.Count;
+                double azRad = frame.Azimuth * Math.PI / 180.0;
+
+                List<PointF> points = new();
+
+                for (int i = 0; i < frame.Distances.Count; i++)
+                {
+                    int d = frame.Distances[i];
+                    if (d <= 0) continue;
+
+                    double angle = (i * step) * Math.PI / 180.0 + azRad;
+                    double dist = d / 2000.0;
+
+                    // === ИСПРАВЛЕНИЕ ОРИЕНТАЦИИ X ===
+                    double wx = frame.X - Math.Cos(angle) * dist;
+                    double wy = frame.Y + Math.Sin(angle) * dist;
+
+                    float px = (float)(cx + wx * POS_SCALE * DIST_SCALE);
+                    float py = (float)(cy - wy * POS_SCALE * DIST_SCALE);
+
+                    points.Add(new PointF(px, py));
+                }
+
+                for (int i = 1; i < points.Count; i++)
+                    g.DrawLine(dashedPen, points[i - 1], points[i]);
+
+                foreach (var p in points)
+                    g.FillEllipse(pointBrush, p.X - 2, p.Y - 2, 4, 4);
+            }
+
+            pictureBox1.Image?.Dispose();
+            pictureBox1.Image = bmp;
+        }
+
+        // ===== STOP =====
+        private void StopAll()
+        {
+            isPaused = false;
+            fileTimer.Stop();
+
+            reader?.Dispose();
+            reader = null;
+
+            cts?.Cancel();
+            udpServer?.Close();
+            udpServer = null;
+        }
+    }
+}
